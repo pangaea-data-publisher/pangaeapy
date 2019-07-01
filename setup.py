08daf5e76eec8582e3cb545dbca1619be858ac4a
huberrob <huberrob@users.noreply.github.com>
2019-07-01 13:58:41 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,11 +7,20 @@ with open("README.md", "r") as fh:
 descr = """
         This module allows to download and analyse metadata as
         well as data from tabular PANGAEA (https://www.pangaea.de) datasets.
+        
+        Usage:
+        import pangaeapy.pandataset as pd
+        ds= pd.PanDataSet(787140)
+        print(ds.title)
+        print(ds.data.head())
+        
+        Please visit the github project page to see more documentation and some examples:
+        https://github.com/pangaea-data-publisher/pangaeapy
         """  
 # Setup
 setuptools.setup(
     name="pangaeapy",
-    version="0.0.2",
+    version="0.0.3",
     author="PANGAEA® - Data Pub­lisher for Earth & En­vir­on­mental Sci­ence,Robert Huber,Markus Stocker,Egor Gordeev",
     author_email="rhuber@uni-bremen.de",
     description=descr,
