cc8f4ce084c87a5365321d443290a64aed261e41
huberrob <huberrob@users.noreply.github.com>
2021-12-23 12:13:26 +0100

    inits

--- pangaeapy/__init__.py
+++ pangaeapy/__init__.py
@@ -1,7 +1,7 @@
 """
-pangaeapy is a package allowing to download and analyse metadata 
+pangaeapy is a package allowing to download and analyse metadata
 as well as data from tabular PANGAEA (https://www.pangaea.de) datasets.
 """
 from pangaeapy.src.pandataset import PanAuthor,PanEvent,PanParam,PanDataSet
-from pangaeapy.src.panquery import PanQuery
+from pangaeapy.src.panquery import  PanQuery
 
