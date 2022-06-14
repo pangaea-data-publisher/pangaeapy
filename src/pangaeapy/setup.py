c9ee266f388750d63ccd47d647c9ae3bb4dad58d
huberrob <huberrob@users.noreply.github.com>
2022-06-14 11:28:06 +0200

    1.0.4

--- src/pangaeapy/setup.py
+++ src/pangaeapy/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pangaeapy',
-    version='1.0.3',
+    version='1.0.4',
     install_requires=['lxml~=4.6.3','requests~=2.26.0','pandas~=1.3.5','numpy>=1.21.0','netcdf4~=1.5.6'],
     packages=['pangaeapy','pangaeapy.exporter'],
     package_dir={'pangaeapy': ''},
