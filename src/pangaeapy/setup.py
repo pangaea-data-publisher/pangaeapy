dde10002c8b0a8999f4b5c79b394ba314335b315
huberrob <huberrob@users.noreply.github.com>
2023-04-12 14:04:35 +0200

    netcfd 1.6.3 to fix python 3.11 issues

--- src/pangaeapy/setup.py
+++ src/pangaeapy/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pangaeapy',
-    version='1.0.13',
+    version='1.0.14',
     install_requires=['lxml>=4.9.1','requests>=2.26.0','pandas>=1.3.5','numpy>=1.21.0','netcdf4~=1.6.3'],
     packages=['pangaeapy','pangaeapy.exporter'],
     package_dir={'pangaeapy': ''},
