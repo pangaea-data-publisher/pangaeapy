40dce4b9972d9d7c745c087615b42a47bed3b103
Johannes Röttenbacher <jr-alias@posteo.de>
2025-03-07 10:36:32 +0100

    Mock up config file for testing
    When a dataset is opened and caching is enabled the cache directory will be saved to the config.toml file. After that every new call to PanDataSet will read the config file for the cache dir and use it if not otherwise specified.
    
    Instead of working on the user's default config file a mockup CONFIG_PATH is created. Thus, PanDataSet will create the config file there.
    
    Further, a switch from tempfile to the pytest built-in tmp_path is implemented.

--- test/test_PanDataSet.py
+++ test/test_PanDataSet.py
@@ -6,8 +6,6 @@
 Test the PanDataSet class
 """
 import os
-import tempfile
-
 from pangaeapy.pandataset import PanDataSet
 
 def test_default_cache_dir():
@@ -16,11 +14,10 @@ def test_default_cache_dir():
     assert os.path.isdir(ds.cache_dir)  # Ensure the directory was created
 
 
-def test_custom_cache_dir():
-    with tempfile.TemporaryDirectory() as tmpdir:
-        ds = PanDataSet(968912, enable_cache=True, cache_dir=tmpdir)
-        assert ds.cache_dir == tmpdir
-        ds.terms_conn.close()  # explicitly close the sqlite database
+def test_custom_cache_dir(tmp_path):
+    ds = PanDataSet(968912, enable_cache=True, cache_dir=tmp_path)
+    assert ds.cache_dir == tmp_path
+    ds.terms_conn.close()  # explicitly close the sqlite database
 
 # def test_download_binary():
     # ds = PanDataSet(944101, enable_cache=True)
