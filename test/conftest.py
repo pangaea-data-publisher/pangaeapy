f58fa8cf5c43dfcad98e8755446dcdc044417ead
Johannes Röttenbacher <jr-alias@posteo.de>
2025-03-07 10:36:32 +0100

    Mock up config file for testing
    When a dataset is opened and caching is enabled the cache directory will be saved to the config.toml file. After that every new call to PanDataSet will read the config file for the cache dir and use it if not otherwise specified.
    
    Instead of working on the user's default config file a mockup CONFIG_PATH is created. Thus, PanDataSet will create the config file there.
    
    Further, a switch from tempfile to the pytest built-in tmp_path is implemented.

---
+++ test/conftest.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+"""
+| *author*: Johannes Röttenbacher
+| *created*: 07.03.2025
+
+Config functions to use in all test scripts
+"""
+import pytest
+import toml
+
+
+@pytest.fixture(scope="session", autouse=True)
+def mock_config_paths(tmp_path_factory):
+    # Create a temporary directory for the entire test session
+    temp_config_dir = tmp_path_factory.mktemp("mock_config")
+    temp_config_path = temp_config_dir / "config.toml"
+
+    # Mock the CONFIG_DIR and CONFIG_PATH
+    with pytest.MonkeyPatch.context() as mp:
+        mp.setattr("pangaeapy.pandataset.PanDataSet.CONFIG_DIR", str(temp_config_dir))
+        mp.setattr("pangaeapy.pandataset.PanDataSet.CONFIG_PATH", str(temp_config_path))
+        yield
+
