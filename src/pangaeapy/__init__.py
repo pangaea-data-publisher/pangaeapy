08b8326815308b0fb7875c6940b807a5c3850b61
Niklas Selke <18034624+nselke@users.noreply.github.com>
2025-03-17 14:15:06 +0100

    added __all__ variable for wildcard imports

--- src/pangaeapy/__init__.py
+++ src/pangaeapy/__init__.py
@@ -2,5 +2,10 @@
 pangaeapy is a package allowing to download and analyse metadata
 as well as data from tabular PANGAEA (https://www.pangaea.de) datasets.
 """
-from pangaeapy.pandataset import PanAuthor,PanEvent,PanParam,PanDataSet
+from pangaeapy.pandataset import PanDataSet
 from pangaeapy.panquery import PanQuery
+
+__all__ = [
+    "PanDataSet",
+    "PanQuery",
+]
