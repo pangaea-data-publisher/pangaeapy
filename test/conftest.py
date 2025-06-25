6cde078470a2d48fb0b878087adc0195c8f4a67a
Johannes Röttenbacher <jr-alias@posteo.de>
2025-06-25 11:45:10 +0200

    Remove CONFIG_DIR and CONFIG_PATH mock

--- test/conftest.py
+++ test/conftest.py
@@ -13,9 +13,3 @@ def mock_config_paths(tmp_path_factory):
     # Create a temporary directory for the entire test session
     temp_config_dir = tmp_path_factory.mktemp("mock_config")
     temp_config_path = temp_config_dir / "config.json"
-
-    # Mock the CONFIG_DIR and CONFIG_PATH
-    with pytest.MonkeyPatch.context() as mp:
-        mp.setattr("pangaeapy.pandataset.PanDataSet.CONFIG_DIR", str(temp_config_dir))
-        mp.setattr("pangaeapy.pandataset.PanDataSet.CONFIG_PATH", str(temp_config_path))
-        yield
