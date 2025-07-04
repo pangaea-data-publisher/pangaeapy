1217bd410d57fd72898c26535f4c0d0b022d8db3
Johannes Röttenbacher <jr-alias@posteo.de>
2025-07-04 09:31:34 +0200

    Remove PanDataHarvester from API Docs

--- src/pangaeapy/__init__.py
+++ src/pangaeapy/__init__.py
@@ -3,9 +3,8 @@ pangaeapy is a package allowing to download and analyse metadata
 as well as data from tabular PANGAEA (https://www.pangaea.de) datasets.
 """
 
-__all__ = ["exporter", "PanDataSet", "PanDataHarvester", "PanQuery"]
+__all__ = ["exporter", "PanDataSet", "PanQuery"]
 
 from pangaeapy import exporter
 from pangaeapy.pandataset import PanDataSet
-from pangaeapy.pandataset import PanDataHarvester
 from pangaeapy.panquery import PanQuery
